7eac160bc978f406b57653172cee9f82697df35a
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-09 14:36:51 +0200

    Added Hours in seconds program

---
+++ HW_2/Hours_in_seconds/HW_2_part_2/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int hours = 30;
+        int minutes = 10;
+        int seconds = 54;
+
+        int totalTimeInSeconds = hours * 3600 + minutes * 60 + seconds;
+
+        Console.WriteLine("Загальний час у секундах: " + totalTimeInSeconds);
+    }
+}
