79a1a3ee52d5aa7db29c695472c9077bbb168b9c
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-09 14:33:08 +0200

    Added HW_2_part_1

---
+++ HW_2/Discount/HW_2_part_1/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        int cost = 532123; 
+        double discountPercentage = 13; 
+
+        var discountAmount = cost * (discountPercentage / 100); 
+
+        Console.WriteLine("Величина знижки: " + discountAmount);
+    }
+}
