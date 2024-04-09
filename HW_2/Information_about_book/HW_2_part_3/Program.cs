d3813c8782aef8bc65e6ae67019f984651ebf48b
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-09 14:46:33 +0200

    Added HW_2_part_3

---
+++ HW_2/Information_about_book/HW_2_part_3/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string title = "Чистий код";
+        string author = "Мартін Роберт";
+        var price = "14.99 $"; 
+
+        string bookInfo = string.Format($"Title: {title}, Author: {author}, Price: {price}");
+
+        Console.WriteLine(bookInfo);
+    }
+}
