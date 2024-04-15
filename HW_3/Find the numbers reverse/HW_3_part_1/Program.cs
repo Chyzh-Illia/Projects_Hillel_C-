24349a77328afaba83383f7578410d6327719d64
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-15 10:00:28 +0200

    Added HW_3_part_1

---
+++ HW_3/Find the numbers reverse/HW_3_part_1/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class FindNumbersReverse
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть тризначне число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // first number
+        int firstDigit = number / 100;
+        // second number
+        int secondDigit = (number / 10) % 10;
+        // third numer
+        int thirdDigit = number % 10;
+
+        int reversedNumber = thirdDigit * 100 + secondDigit * 10 + firstDigit;
+
+        Console.WriteLine("Число, отримане під час прочитання його цифр справа наліво: " + reversedNumber);
+    }
+}
