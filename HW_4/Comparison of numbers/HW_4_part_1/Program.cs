c9cff520170a2fa1e245e39172647b14c88b3406
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-15 09:35:00 +0200

    Added the first task from HW_4

---
+++ HW_4/Comparison of numbers/HW_4_part_1/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class ComparisonOfNumbers
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first number:");
+        int number1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        int number2 = int.Parse(Console.ReadLine());
+
+        if (number1 == number2)
+        {
+            Console.WriteLine("The entered numbers are equal.");
+        }
+        else if (number1 > number2)
+        {
+            Console.WriteLine("The first number is greater than the second number.");
+        }
+        else
+        {
+            Console.WriteLine("The second number is greater than the first number.");
+        }
+    }
+}
