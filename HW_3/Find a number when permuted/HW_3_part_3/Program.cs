440b9cdcc00b76703857e9c21b807ca51c3210a6
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-15 10:06:42 +0200

    Added HW_3_Part_3

---
+++ HW_3/Find a number when permuted/HW_3_part_3/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class FindNumberPermuted
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Entering the number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int firstDigit = number / 100;
+        int secondDigit = (number / 10) % 10;
+        int thirdDigit = number % 10;
+
+        int resultNumber = secondDigit * 100 + firstDigit * 10 + thirdDigit;
+
+        Console.WriteLine("The number obtained when permuting the first and second digits: " + resultNumber);
+    }
+}
