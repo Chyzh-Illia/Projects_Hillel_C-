7d548430325bdbd85778f4220d41914dbfd40595
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-15 10:13:28 +0200

    Added HW_3_part_5

---
+++ HW_3/Circle radius/HW_3_part_5/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class CircleRadius
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the radius of the circle:");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        double area = Math.PI * Math.Pow(radius, 2);
+
+        Console.WriteLine("Area of ​​a circle with radius {0} is equal to {1}", radius, area);
+    }
+}
