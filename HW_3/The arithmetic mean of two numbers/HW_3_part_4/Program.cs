34606c1dcae1191ba3919b4029e6c1b2ccc2926e
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-15 10:10:14 +0200

    Added HW_3_part_4

---
+++ HW_3/The arithmetic mean of two numbers/HW_3_part_4/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class TheArithmeticMeanOfTwoNumbers
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first number:");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter second number:");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        double average = (number1 + number2) / 2;
+
+        Console.WriteLine("Arithmetic average of numbers {0} and {1} is equal to {2}", number1, number2, average);
+    }
+}
