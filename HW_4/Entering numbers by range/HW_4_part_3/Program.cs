00902611220a3df0ae47e8bc2c028653d1ac1d5d
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-15 09:53:04 +0200

    Added part 3 from HW_4

---
+++ HW_4/Entering numbers by range/HW_4_part_3/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class EnteringNumbersByRange
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number from 1 to 100:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number < 1 || number > 100)
+        {
+            Console.WriteLine("The number must be between 1 and 100");
+            return;
+        }
+
+        if (number % 15 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if (number % 3 == 0)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if (number % 5 == 0)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
