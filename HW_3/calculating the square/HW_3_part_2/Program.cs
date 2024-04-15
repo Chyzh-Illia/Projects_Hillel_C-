ee6e2fe75829e9caec8f62f3dc83851855f144df
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-15 10:03:13 +0200

    Added HW_3_part_2

---
+++ HW_3/calculating the square/HW_3_part_2/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class CalculatingTheSquare
+{
+    private static void Main(string[] args)
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter number:");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            double square = Math.Pow(number, 2);
+
+            Console.WriteLine("The square of the number {0} is equal to {1}", number, square);
+        }
+    }
+}
