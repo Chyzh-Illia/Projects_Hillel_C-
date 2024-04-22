ca9c9ff747912f8e4ee81580d4e9b3358ff38b0e
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-22 09:46:12 +0200

    Added HW_6_part_2

---
+++ HW_6/Array inverison/HW_6_part_2/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class InversionArray
+{
+    private static void Main(string[] args)
+    {
+        int[] array = { 5, -9, 8, 7 };
+
+        
+        for (int i = 0; i < array.Length / 2; i++)
+        {
+            int temp = array[i];
+            array[i] = array[array.Length - 1 - i];
+            array[array.Length - 1 - i] = temp;
+        }
+
+        
+        Console.Write("Inverted array (method 2): ");
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
