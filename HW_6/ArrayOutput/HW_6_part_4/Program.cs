02a41795dd0e10958a3e5018aa0b9cb370fdae47
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-22 09:53:17 +0200

    Added_HW_6_part_4

---
+++ HW_6/ArrayOutput/HW_6_part_4/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Random rand = new Random();
+
+        int[] array = new int[20];
+
+        Console.Write("Array: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(-5, 6);
+            Console.Write(array[i] + " ");
+
+            if (array[i] == -1)
+            {
+                break;
+            }
+        }
+        Console.WriteLine();
+    }
+}
