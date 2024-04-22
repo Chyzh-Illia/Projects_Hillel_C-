7a33599e15b9b49678571d6e4faff82624b16096
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-22 09:50:42 +0200

    Added_HW_6_part_3

---
+++ HW_6/ArrayRandom/HW_6_part_3/Program.cs
@@ -0,0 +1,36 @@
+﻿public class ArrayRandom
+{
+    public static void Main(string[] args)
+    {
+        Random rand = new Random();
+
+        int[] array = new int[10];
+
+        Console.Write("Array to add element: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(-100, 101);
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+
+        int newElement = rand.Next(-100, 101);
+        Console.WriteLine("The element we add to the array: " + newElement);
+
+        int[] newArray = new int[array.Length + 1];
+
+        newArray[0] = newElement;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            newArray[i + 1] = array[i];
+        }
+
+        Console.Write("Array after adding an element: ");
+        for (int i = 0; i < newArray.Length; i++)
+        {
+            Console.Write(newArray[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
