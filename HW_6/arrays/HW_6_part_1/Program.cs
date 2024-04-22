5f4679b67d8e9456760afe14787f52e0af7b9053
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-04-22 09:42:38 +0200

    Added HW_6_part_1

---
+++ HW_6/arrays/HW_6_part_1/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Array
+{
+    private static void Main(string[] args)
+    {
+        Random rand = new Random();
+
+        int[] array = new int[10];
+
+        Console.Write("Array: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(-100, 101);
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+
+        int positiveCount = 0;
+        foreach (int num in array)
+        {
+            if (num > 0)
+            {
+                positiveCount++;
+            }
+        }
+
+        Console.WriteLine("Number of positive numbers: " + positiveCount);
+    }
+}
