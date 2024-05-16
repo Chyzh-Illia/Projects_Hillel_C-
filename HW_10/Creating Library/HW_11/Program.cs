5adf4202748acbbdd4b3267f860f3968fa8c4ad7
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-05-16 14:59:01 +0200

    Add the first programm Creating Library

---
+++ HW_10/Creating Library/HW_11/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class UkrainianStringUtils
+{
+    public static int CountVowels(string input)
+    {
+        int count = 0;
+        string vowels = "аеєиіїоуюя";
+        foreach (char ch in input.ToLower())
+        {
+            if (vowels.Contains(ch))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int CountConsonants(string input)
+    {
+        int count = 0;
+        string consonants = "бвгґджзйклмнпрстфхцчшщ";
+        foreach (char ch in input.ToLower())
+        {
+            if (consonants.Contains(ch))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    public static string RemoveDuplicates(string input)
+    {
+        string result = "";
+        foreach (char ch in input)
+        {
+            if (result.IndexOf(ch) == -1)
+            {
+                result += ch;
+            }
+        }
+        return result;
+    }
+
+    public static string RemovePunctuation(string input)
+    {
+        return Regex.Replace(input, @"[\p{P}\p{S}]", "");
+    }
+
+}
