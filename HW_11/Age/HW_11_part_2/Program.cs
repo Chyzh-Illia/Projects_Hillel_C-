285987bed7477de7b1abfbfead780c34727350f5
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-05-16 15:03:32 +0200

    Add Age program

---
+++ HW_11/Age/HW_11_part_2/Program.cs
@@ -0,0 +1,24 @@
+﻿class Age
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Введіть ваш вік: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 0)
+            {
+                throw new FormatException("Вік не може бути від'ємним числом.");
+            }
+
+            Console.WriteLine($"Ваш вік: {age}");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine($"Помилка: {ex.Message}");
+        }
+
+        Console.ReadLine();
+    }
+}
