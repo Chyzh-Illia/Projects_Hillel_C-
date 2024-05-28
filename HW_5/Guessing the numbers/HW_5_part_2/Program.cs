bd2e55365e4a44dfb03ccaee3577de3ab10589ca
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-05-28 09:45:11 +0200

    Add program

---
+++ HW_5/Guessing the numbers/HW_5_part_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int numberToGuess = random.Next(1, 147);
+            int userGuess = 0;
+
+            Console.WriteLine("Я загадав число від 1 до 146. Спробуйте його вгадати.");
+
+            while (userGuess != numberToGuess)
+            {
+                Console.Write("Ваше припущення: ");
+                if (!int.TryParse(Console.ReadLine(), out userGuess))
+                {
+                    Console.WriteLine("Будь ласка, введіть число.");
+                    continue;
+                }
+
+                if (userGuess < numberToGuess)
+                {
+                    Console.WriteLine("Більше.");
+                }
+                else if (userGuess > numberToGuess)
+                {
+                    Console.WriteLine("Менше.");
+                }
+            }
+
+            Console.WriteLine("Вітаю! Ви вгадали число.");
+        }
+    }
+}
