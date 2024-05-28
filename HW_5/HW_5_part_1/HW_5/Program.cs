b4451bd487d785cf95bfedcd4f87403cd94228d2
DESKTOP-P7L3VOK\chyzh <illiac@nayax.com>
2024-05-28 09:43:49 +0200

    Add verify password

---
+++ HW_5/HW_5_part_1/HW_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PasswordCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string correctPassword = "root";
+            string input;
+
+            do
+            {
+                Console.Write("Enter the password: ");
+                input = Console.ReadLine();
+                if (input != correctPassword)
+                {
+                    Console.WriteLine("Неправильний пароль!");
+                }
+            } while (input != correctPassword);
+
+            Console.WriteLine("Access granted.");
+        }
+    }
+}
